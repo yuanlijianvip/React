c192c4b6f4a4d7e9f998c4527594b89ae18301f8
yuanlijian <yuanlijianvip@163.com>
2020-05-14 23:54:04 +0800

    Action和Reducer的编写

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -8,13 +8,23 @@ class TodoList extends Component {
         super(props);
 
         this.state = store.getState();
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        store.subscribe(this.handleStoreChange);
     }
 
     render() {
         return (
             <div style={{marginTop: '10px', marginLeft: '10px'}}>
-                <Input placeholder='todo info' value={this.state.inputValue} style={{width: '300px', marginRight: '10px'}}/>
-                <Button type="primary">提交</Button>
+                <Input 
+                    placeholder='todo info' 
+                    value={this.state.inputValue} 
+                    style={{width: '300px', marginRight: '10px'}}
+                    onChange={this.handleInputChange}
+                />
+                <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
                 <List
                     style={{marginTop: '10px', width: '300px'}}
                     bordered
@@ -27,6 +37,27 @@ class TodoList extends Component {
                 />
             </div>
         );
+    }
+
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action);
+        console.log(e.target.value);
+    }
+
+    handleStoreChange() {
+        this.setState(store.getState());
+    }
+
+    handleBtnClick() {
+        const action = {
+            type: 'add_todo_item'
+        }
+
+        store.dispatch(action);
     }
 }
 
