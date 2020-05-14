5443a99df39ccb2b18c8baed86e86f884dc59181
yuanlijian <yuanlijianvip@163.com>
2020-05-15 01:09:34 +0800

    补充

--- todolist/src/store/reducer.js
+++ todolist/src/store/reducer.js
@@ -6,6 +6,7 @@ const defaultState = {
 }
 
 //reducer 可以接受state,但是绝不能修改state
+//纯函数指的是，给定固定的输入，就一定会有固定的输出，而且不会有任何副作用
 export default (state = defaultState, action) => {
     if (action.type === CHANGE_INPUT_VALUE) {
         const newState = JSON.parse(JSON.stringify(state));
